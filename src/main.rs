4e9717b3ff883387ffbccbb486b85c4f6695c2fb
Henry Dong <henry@henrys-air.attlocal.net>
2022-08-05 17:00:58 -0500

    refactored code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,154 @@
 use std::io;
 use std::{i32};
 
+fn selection(){
+    println!("_____________________");
+    println!("Input 0 For Addition");
+    println!("Input 1 For Subtraction");
+    println!("Input 2 For Multiplication");
+    println!("Input 3 For Division");
+    println!("Input 4 For Mod");
+    println!("Input 5 for Exponents");
+    println!("Input 6 to calculate the slope of 2 points");
+    println!("Input 99 for help");
+    println!("_____________________");
 
+}
+
+
+fn addition(a: i128,b: i128 ) {
+    println!("You have choosen Addition");
+    println!("_____________________");
+    println!("Your Number is {}", a + b);
+}
+
+fn subtraction(a: i128, b: i128){
+    println!("You Have Choosen Subtraction");
+    println!("_____________________");
+    println!("Your Number is {}", a - b);
+}
+fn multiplication(a: i128, b: i128){
+    println!("You Have Choosen Multiplication");
+    println!("_____________________");
+    println!("Your Number is {}", a * b);
+}
+fn division(a: i128, b:i128) {
+    println!("You Have Choosen Division");
+    println!("_____________________");
+    let is_whole = a/b;
+
+    if is_whole == 0 {
+        println!("{}",a);
+        println!("-");
+        println!("{}",b);
+    }
+    else{
+        println!("Your Number is {}", a / b);
+    }
+    
+
+}
+fn modulo(a: i128, b: i128){
+    println!("You Have Choosen Mod");
+    println!("_____________________");
+    println!("Your Number is {}", a % b);
+}
+fn exponent(a: i128, b: i128){
+    println!("You have choosen Exponents");
+    println!("NOTE: Since this rust program uses u128 numbers, there is a limit of 3.40282367e38 - 1, which is 340282366920938463463374607431768211456");
+    println!("I could switch to u128, but thats another project for another time");
+    println!("_____________________");
+        
+    println!("We will use your A value as the base");
+    println!("Your base value is: {}",a);
+    println!("We are going to use your B value as the exponent");
+    println!("Your exponent value is: {}",b);
+    println!("_____________________");
+
+
+    let base = u128::pow(a.try_into().unwrap(),b.try_into().unwrap());
+
+    println!("{}",base)
+
+
+}
+fn slope(){
+    println!("You Have choosen Slope");
+    println!("_____________________");
+       
+        
+    let mut x1 = String::new();
+
+    let mut x2 = String::new();
+
+    let mut y1 = String::new();
+
+    let mut y2 = String::new();
+
+
+    println!("Please Input Your x1 value");
+    io::stdin()
+    .read_line(&mut x1)
+    .expect("Failed to read line");
+    let x1: i32 = x1.trim().parse().ok().expect("Input not an integer");
+    println!("_____________________");
+
+    println!("Please Input your y1 value");
+    io::stdin()
+    .read_line(&mut y1)
+    .expect("Failed to read line");
+    let y1: i32 = y1.trim().parse().ok().expect("Input not an integer");
+    println!("_____________________");
+
+    println!("Please Input your x2 value");
+    io::stdin()
+    .read_line(&mut x2)
+    .expect("Failed to read line");
+    let x2: i32 = x2.trim().parse().ok().expect("Input not an integer");
+    println!("_____________________");
+
+    println!("Please Input your y2 value");
+    io::stdin()
+    .read_line(&mut y2)
+    .expect("Failed to read line");
+    let y2: i32 = y2.trim().parse().ok().expect("Input not an integer");
+    println!("_____________________");
+
+    let x3 = x2 - x1;
+
+    let y3 = y2 - y1;
+
+    if x3 == 0 || y3 == 0{
+        println!("_____________________");
+        println!("The slope is 0");
+        println!("The program will abort if the quotent will be 0, this way we don't fail to divide by 0");
+        println!("_____________________");
+        std::process::abort();
+    }
+ 
+    let final_slope = y3 / x3;
+
+    if final_slope == 0{
+        println!("_____________________");
+       println!("Your slope is:");
+       println!("{}",y3);
+       println!("______");
+       println!("{}",x3);
+       println!("_____________________");
+
+    } else {
+        println!("_____________________");
+        println!("Your Slope is {}", final_slope);
+        println!("_____________________");
+    }
+
+
+}
+fn help(){
+    println!("_____________________");
+    println!("This is the help page");
+    println!("_____________________");
+}
 
 fn main() {
     let mut calc_option = String::new();
@@ -11,149 +158,61 @@ fn main() {
     let mut b = String::new(); 
 
 
+    println!("_____________________");
     println!("Input Your First Number");
+    println!("_____________________");
 
     io::stdin()
     .read_line(&mut a)
     .expect("Failed to read line");
 
-    let a: i32 = a.trim().parse().ok().expect("Input not an integer");
+    let a: i128 = a.trim().parse().ok().expect("Input not an integer");
 
+    println!("_____________________");
     println!("Input Your Second Number");
+    println!("_____________________");
 
     io::stdin()
     .read_line(&mut b)
     .expect("Failed to read line");
-    let b: i32 = b.trim().parse().ok().expect("Input not an integer");
+    let b: i128 = b.trim().parse().ok().expect("Input not an integer");
 
-    println!("Input 0 For Addition");
-    println!("Input 1 For Subtraction");
-    println!("Input 2 For Multiplication");
-    println!("Input 3 For Division");
-    println!("Input 4 For Mod");
-    println!("Input 5 for Exponents");
-    println!("Input 6 to calculate the slope of 2 points");
-
-
-
-    io::stdin()
+    selection();
+    
+     io::stdin()
     .read_line(&mut calc_option)
     .expect("Failed to read line");
 
     let x: i32 = calc_option.trim().parse().expect("Input not an integer");
     if x == 0{
-       println!("You have choosen Addition");
-
-       println!("Your Number is {}", a + b);
-        
-    }
+        addition(a,b);
+     }
 
     if x == 1{
-        println!("You Have Choosen Subtraction");
-        println!("Your Number is {}", a - b);
+        subtraction(a,b);
     }
 
     if x == 2{
-        println!("You Have Choosen Multiplication");
-        println!("Your Number is {}", a * b);
+        multiplication(a,b);
     }
 
     if x == 3{
-        println!("You Have Choosen Division");
-        println!("If the number is a fraction or a decimal, This program will return 0");
-        println!("Your Number is {}", a / b);
+        division(a,b);
     }
 
     if x == 4{
-        println!("You Have Choosen Mod");
-        println!("Your Number is {}", a % b);
+        modulo(a,b);
     }
 
     if x == 5{
-        println!("You have choosen Exponents");
-        
-        let mut base = String::new();
-        let mut exponent = String::new();
-
-        println!("Please Input Your Base value");
-        io::stdin()
-        .read_line(&mut base)
-        .expect("Failed to read line");
-        let base: i32 = base.trim().parse().ok().expect("Input not an integer");
-
-        println!("Please Input Your Exponent value");
-        io::stdin()
-        .read_line(&mut exponent)
-        .expect("Failed to read line");
-        let exponent: i32 = exponent.trim().parse().ok().expect("Input not an integer");
-
-        let base = i32::pow(base,exponent.try_into().unwrap());
-
-        println!("{}",base)
-
-
+        exponent(a,b);
     }
 
     if x == 6 {
-        println!("You Have choosen Slope");
-       
-        
-        let mut x1 = String::new();
-
-        let mut x2 = String::new();
-    
-        let mut y1 = String::new();
-    
-        let mut y2 = String::new();
-
-
-        println!("Please Input Your x1 value");
-        io::stdin()
-        .read_line(&mut x1)
-        .expect("Failed to read line");
-        let x1: i32 = x1.trim().parse().ok().expect("Input not an integer");
-
-        println!("Please Input your y1 value");
-        io::stdin()
-        .read_line(&mut y1)
-        .expect("Failed to read line");
-        let y1: i32 = y1.trim().parse().ok().expect("Input not an integer");
-
-        println!("Please Input your x2 value");
-        io::stdin()
-        .read_line(&mut x2)
-        .expect("Failed to read line");
-        let x2: i32 = x2.trim().parse().ok().expect("Input not an integer");
-
-        println!("Please Input your y2 value");
-        io::stdin()
-        .read_line(&mut y2)
-        .expect("Failed to read line");
-        let y2: i32 = y2.trim().parse().ok().expect("Input not an integer");
-
-        let x3 = x2 - x1;
-
-        let y3 = y2 - y1;
-
-        if x3 == 0 || y3 == 0{
-            println!("The slope is 0");
-            println!("The program will abort if the quotent will be 0, this way we don't fail to divide by 0");
-            std::process::abort();
-        }
-     
-        let final_slope = y3 / x3;
-
-        if final_slope == 0{
-           println!("Your slope is:");
-           println!("{}",y3);
-           println!("______");
-           println!("{}",x3);
-
-        } else {
-            println!("Your Slope is {}", final_slope);
-        }
-
-
+        slope();
+    }
+    if x == 99 {
+        help();
     }
 
 }
